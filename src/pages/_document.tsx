f69e97ad9948b7bb18c5a071cf0ac078bcb7ce1a
bahobab@hotmail.com <bahobab@hotmail.com>
2021-06-10 14:34:46 -0500

    added favicon.jpg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-          <link rel="icon" href="/media/img/favicon.ico">
+          <link rel="shortcut icon" href="/favicon.ico">
         </Head>
 
         <body>
