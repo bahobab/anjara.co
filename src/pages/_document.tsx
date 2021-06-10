30b68e8ad4054850cca351e3fd96c8ac681e0a66
bahobab@hotmail.com <bahobab@hotmail.com>
2021-06-10 15:39:25 -0500

    format 13 criteres

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-          <link rel="icon" type="image/x-icon" href="/favicon.ico">
         </Head>
 
         <body>
